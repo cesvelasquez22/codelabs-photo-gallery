ed17bcec6afd905cc8869c2ecf8d1125b9af3b37
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-11-10 21:42:32 -0600

    + add feedback chat component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FeedbackAndChatComponent } from './feedback-and-chat/feedback-and-chat.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FeedbackAndChatComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true
